c37cde1357f94ecba1b08f7eeec7c25c640b6953
EC2 Default User <ec2-user@ip-172-31-9-234.ec2.internal>
2020-11-26 03:09:45 +0000

    Agregar archivos para cicd taller 14

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, json, Response, request
 from flask_cors import CORS
+import stressypy
 
 # A very basic API created using Flask that has two possible routes for requests.
 
@@ -10,7 +11,7 @@ CORS(app)
 # sent to the service root will receive a healthy response.
 @app.route("/")
 def healthCheckResponse():
-    return jsonify({"message": "Version 2. Nothing here, used for health check. Try /mysfits instead."})
+    return jsonify({"message": "Version 1. Nothing here, used for health check. Try /mysfits instead."})
 
 # The main API resource that the next version of the Mythical Mysfits website
 # will utilize. It returns the data for all of the Mysfits to be displayed on
@@ -21,7 +22,7 @@ def healthCheckResponse():
 def getMysfits():
 
     # read the mysfits JSON from the listed file.
-    response = Response(open("mysfits-response-v2.json").read())
+    response = Response(open("mysfits-response.json").read())
 
     # set the Content-Type header so that the browser is aware that the response
     # is formatted as JSON and our frontend JavaScript code is able to
@@ -29,3 +30,13 @@ def getMysfits():
     response.headers["Content-Type"] = "application/json"
 
     return response
+
+@app.route("/error")
+def error():
+    raise Exception('error endpoint')
+
+@app.route("/load")
+def load():
+    load_job = stressypy.create_job(1, 10)
+    load_job.run()
+    return jsonify({"message": "Load delivered"})
